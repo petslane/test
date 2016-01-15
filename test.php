3d766233ea6f85c14e0d94b2582600623f116a48
petslane <peeter.normak@eesti.ee>
2016-01-15 22:46:58 +0200

    test

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Test class
+ */
+class test {
+
+	/**
+	 * Test function
+	 *
+	 * @param string $world Test string
+	 */
+	public function hello($world='!') {
+		echo $world;
+	}
+}
